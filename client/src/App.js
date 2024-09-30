e0c03af809bfb1de3ca3dae40a608c57846e135b
Jerry Qian <jerryqian12@gmail.com>
2024-09-30 15:00:58 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,64 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Body">
+      <div className="blogTitle">
+        <h1>Blog title goes here </h1>
+        <div className="subTitle">
+          <div className="left">
+            Company Name
+          </div>
+          <div className="right">
+            Job Position
+          </div>
+        </div>
+      </div>
+      <div className="bodyContent">
+        <div className="mainContent">
+          <p>This is content about the blog title. Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+          <div className="mediaContent">
+            <img src="./temp.jpeg"/>
+          </div>
+          <div className="contentBottom">
+            <div className="leftCB">
+              upload file button will go here if poster<br/>
+              otherwise like and dislike btn
+            </div>
+            <div className="rightCB">
+              <div className="assessmentType"> 
+              Assessment Type/Tag goes here, Author goes here <br/>
+              Created on date, last edited date
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="commentAndUsers">
+        <div className="CAUWrapper">
+          <div className="commentSection">
+            <div className="comment">
+              <div className="commentContent">
+               <p>Comment content Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+              </div>
+              <div className="commentInfo">
+                <div className="CILeft">
+                  author
+                </div>
+                <div className="CIRight">
+                  date posted/edited
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="similarUsers">
+            <ul>
+              <li>User 1</li>
+              <li>User 2</li>
+              <li>User 3</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
