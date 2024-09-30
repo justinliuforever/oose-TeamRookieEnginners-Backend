5978500bf48671728a5a6b400116020c6274e687
Jerry Qian <jerryqian12@gmail.com>
2024-09-30 18:08:52 -0400

    Uploaded client files -- get from server working okay, post has slight problems

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,14 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [blog, setBlog] = useState([]);
+  const [blogs, setBlog] = useState([]);
   const [blogDetails, setBlogDetails] = useState({
     title: '',
     companyName: '',
     jobTitle: '',
     content: '',
     assessmentType: '',
-    author: '',
-    fileURL: '',
+    author:''
   });
 
   useEffect(() => {
@@ -26,7 +25,23 @@ function App() {
       const response = await axios.get('http://localhost:5555/api/blogs');
       console.log("all okay");
       console.log(response.data.data[0]);
-      setBlog(response.data.data[0]);
+      setBlog(response.data.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const addBlog = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5555/api/blogs', JSON.stringify(blogDetails));
+      setBlog([...blogs, response.data]); // Update the state with the new book
+      setBlogDetails({ title: '',
+        companyName: '',
+        jobTitle: '',
+        content: '',
+        assessmentType: '',
+        author: ''}); 
     } catch (err) {
       console.error(err);
     }
@@ -34,64 +49,121 @@ function App() {
 
   return (
     <div className="Body">
-      <div className="blogTitle">
-        <h1>{blog.title} </h1>
-        <div className="subTitle">
-          <div className="left">
-            {blog.companyName}
-          </div>
-          <div className="right">
-            {blog.jobTitle}
+      <div className="formWrapper">
+          <form onSubmit={addBlog}>
+            <input
+              type="text"
+              placeholder="Title"
+              value={blogDetails.title}
+              onChange={(e) => setBlogDetails({ ...blogDetails, title: e.target.value })}
+              required
+              className="titleInput"
+            />
+            <input
+              type="text"
+              placeholder="Company name"
+              value={blogDetails.companyName}
+              onChange={(e) => setBlogDetails({ ...blogDetails, companyName: e.target.value })}
+              required
+              className="titleInput"
+            />
+            <input
+              type="text"
+              placeholder="Job Title"
+              value={blogDetails.jobTitle}
+              onChange={(e) => setBlogDetails({ ...blogDetails, jobTitle: e.target.value })}
+              required
+              className="titleInput"
+            />
+            <input
+              type="text"
+              placeholder="Content"
+              value={blogDetails.content}
+              onChange={(e) => setBlogDetails({ ...blogDetails, content: e.target.value })}
+              required
+              className="titleInput"
+            />
+            <input
+              type="text"
+              placeholder="assessmentType"
+              value={blogDetails.assessmentType}
+              onChange={(e) => setBlogDetails({ ...blogDetails, assessmentType: e.target.value })}
+              required
+              className="titleInput"
+            />
+            <input
+              type="text"
+              placeholder="Author"
+              value={blogDetails.author}
+              onChange={(e) => setBlogDetails({ ...blogDetails, author: e.target.value })}
+              required
+              className="titleInput"
+            />
+            <button type="submit" className="submitBtn">Add Blog</button>
+          </form>
+        </div>
+
+      {blogs.map((blog) => ( <div className="wrapperForTemp">
+        <div className="blogTitle">
+          <h1>{blog.title} </h1>
+          <div className="subTitle">
+            <div className="left">
+              {blog.companyName}
+            </div>
+            <div className="right">
+              {blog.jobTitle}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="bodyContent">
-        <div className="mainContent">
-          <p>{blog.content}</p>
-          <div className="mediaContent">
-          File upload result here
-            <img src="./temp.jpeg"/>
-          </div>
-          <div className="contentBottom">
-            <div className="leftCB">
-              upload file button will go here if poster<br/>
-              <button>Like</button> <button>Dislike</button>
+        <div className="bodyContent">
+          <div className="mainContent">
+            <p>{blog.content}</p>
+            <div className="mediaContent">
+            File upload result here
+              <img src="./temp.jpeg"/>
             </div>
-            <div className="rightCB">
-              <div className="assessmentType"> 
-              {blog.assessmentType}, {blog.author} <br/>
-              Created on date/last edited date
+            <div className="contentBottom">
+              <div className="leftCB">
+                upload file button will go here if poster<br/>
+                <button>Like</button> <button>Dislike</button>
               </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="commentAndUsers">
-        <div className="CAUWrapper">
-          <div className="commentSection">
-            <div className="comment">
-              <div className="commentContent">
-               <p>Comment content Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
-              </div>
-              <div className="commentInfo">
-                <div className="CILeft">
-                  author
+              <div className="rightCB">
+                <div className="assessmentType"> 
+                {blog.assessmentType}, {blog.author} <br/>
+                {blog.blogCreatedAt}
                 </div>
-                <div className="CIRight">
-                  date posted/edited
-                </div>
               </div>
             </div>
           </div>
-          <div className="similarUsers">
-            <ul>
-              <li>User 1</li>
-              <li>User 2</li>
-              <li>User 3</li>
-            </ul>
+        </div>
+        <div className="commentAndUsers">
+          <div className="CAUWrapper">
+            <div className="commentSection">
+              <div className="comment">
+                <div className="commentContent">
+                 <p>Comment content Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+                </div>
+                <div className="commentInfo">
+                  <div className="CILeft">
+                    author
+                  </div>
+                  <div className="CIRight">
+                    date posted/edited
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="similarUsers">
+              <ul>
+                <li>User 1</li>
+                <li>User 2</li>
+                <li>User 3</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
+      ))}
     </div>
   );
 }
